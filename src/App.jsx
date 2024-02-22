32299ac7c84d30793dc305f8809d004a126212a2
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-02-22 18:40:51 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Gallery from "./components/Gallery";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
